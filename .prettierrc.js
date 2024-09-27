32f4cfe0e9d1905e3f8648f3fea974e761dc02e5
Michal Kula <kula@devanddeliver.com>
2024-09-27 21:31:10 +0200

    chore: nvm to 20, npm to yarn, eslint and prettier plugins setup

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	endOfLine: 'auto',
+	printWidth: 100,
+	plugins: [require('prettier-plugin-tailwindcss')],
+};
